9ba39feddb31743e4ab102ffa2d39ca80ca69747
sarahbbirne <154691526+sarahbbirne@users.noreply.github.com>
2024-02-21 19:14:56 -0300

    Update and rename aula11.py to imc_calculator.py

--- aula11.py
+++
@@ -1,8 +0,0 @@
-#IMC CALCULATOR 
-nome = "Felipe Memento"
-altura = 1.73
-peso = 70
-imc = peso // altura ** 2
-
-print (nome, "tem", altura, "de altura")
-print ("pesa", peso,"kg e seu imc é:", imc)
---
+++ imc_calculator.py
@@ -0,0 +1,8 @@
+#IMC CALCULATOR 
+nome = "Sarah Birne"
+altura = 1.60
+peso = 60
+imc = peso // altura ** 2
+
+print (nome, "tem", altura, "de altura")
+print ("pesa", peso,"kg e seu imc é:", imc)
